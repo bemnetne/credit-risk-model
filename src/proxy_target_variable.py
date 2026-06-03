09e7a1b28d81c893dd28cfa0b99e6c72ba8ad2e8
bemnet <bemnet.negash15@gmail.com>
2026-06-03 23:43:24 +0300

    feat: add RFM clustering and high-risk target variable creation

---
+++ src/proxy_target_variable.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+
+def calculate_rfm(df):
+    """
+    Calculate Recency, Frequency and Monetary metrics.
+    """
+
+    df = df.copy()
+
+    df["TransactionStartTime"] = pd.to_datetime(
+        df["TransactionStartTime"]
+    )
+
+    snapshot_date = (
+        df["TransactionStartTime"].max()
+        + pd.Timedelta(days=1)
+    )
+
+    rfm = (
+        df.groupby("CustomerId")
+        .agg(
+            Recency=(
+                "TransactionStartTime",
+                lambda x: (
+                    snapshot_date - x.max()
+                ).days
+            ),
+            Frequency=(
+                "TransactionId",
+                "count"
+            ),
+            Monetary=(
+                "Value",
+                "sum"
+            )
+        )
+        .reset_index()
+    )
+
+    return rfm
